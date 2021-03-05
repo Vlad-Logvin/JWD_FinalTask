be48d80042a51738d09777210348a52eee6db472
Vlad Logvin <vladlogvin97@gmail.com>
2021-03-05 13:10:09 +0300

    Add ConnectionPool

---
+++ src/main/java/by/logvin/onlinestore/dao/connectionpool/ConnectionPool.java
@@ -0,0 +1,10 @@
+package by.logvin.onlinestore.dao.connectionpool;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ConnectionPool {
+
+}
