9973fc15910ee5143c0bf3d6e29a5bbc8a1ef2a9
Vlad Logvin <vladlogvin97@gmail.com>
2021-03-07 23:50:34 +0300

    add UserServiceImpl

--- src/main/java/by/logvin/onlinestore/service/ServiceProvider.java
+++ src/main/java/by/logvin/onlinestore/service/ServiceProvider.java
@@ -1,4 +1,21 @@
 package by.logvin.onlinestore.service;
 
+import by.logvin.onlinestore.service.impl.UserServiceImpl;
+
 public class ServiceProvider {
+    public final static ServiceProvider instance = new ServiceProvider();
+
+    private ServiceProvider(){
+
+    }
+
+    private final UserService userService = new UserServiceImpl();
+
+    public ServiceProvider getInstance(){
+        return instance;
+    }
+
+    public UserService getUserService(){
+        return userService;
+    }
 }
--- src/main/java/by/logvin/onlinestore/service/impl/UserServiceImpl.java
+++ src/main/java/by/logvin/onlinestore/service/impl/UserServiceImpl.java
@@ -2,17 +2,54 @@ package by.logvin.onlinestore.service.impl;
 
 import by.logvin.onlinestore.bean.RegistrationInfo;
 import by.logvin.onlinestore.bean.User;
+import by.logvin.onlinestore.dao.DAOProvider;
+import by.logvin.onlinestore.dao.UserDAO;
+import by.logvin.onlinestore.dao.exception.DAOException;
 import by.logvin.onlinestore.service.UserService;
 import by.logvin.onlinestore.service.exception.ServiceException;
+import by.logvin.onlinestore.service.validator.ValidatorProvider;
+import by.logvin.onlinestore.service.validator.exception.ValidatorException;
 
 public class UserServiceImpl implements UserService {
     @Override
     public boolean signUp(RegistrationInfo info) throws ServiceException {
-        return false;
+
+        try {
+            if(ValidatorProvider.getInstance().getAuthorizationValidator().validate(info)){
+                throw new ServiceException("Wrong login or password");
+            }
+        } catch (ValidatorException e) {
+            throw new ServiceException("Validation error", e);
+        }
+
+        UserDAO userDAO = DAOProvider.getInstance().getUserDAO();
+        boolean b = false;
+        try {
+            b = userDAO.signUp(info);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return b;
     }
 
     @Override
     public User signIn(String login, String password) throws ServiceException {
-        return null;
+        try {
+            if(ValidatorProvider.getInstance().getAuthorizationValidator().validate(login, password)){
+                throw new ServiceException("Wrong login or password");
+            }
+        } catch (ValidatorException e) {
+            throw new ServiceException("Validation error", e);
+        }
+
+
+        UserDAO userDAO = DAOProvider.getInstance().getUserDAO();
+        User user = null;
+        try {
+            user = userDAO.signIn(login, password);
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+        return user;
     }
 }
