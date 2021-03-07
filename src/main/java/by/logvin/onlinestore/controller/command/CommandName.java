249a2d5b955713d797540011c4ab2d75e506dad7
Vlad Logvin <vladlogvin97@gmail.com>
2021-03-07 23:45:30 +0300

    command support

--- src/main/java/by/logvin/onlinestore/controller/command/CommandName.java
+++ src/main/java/by/logvin/onlinestore/controller/command/CommandName.java
@@ -1,5 +1,5 @@
 package by.logvin.onlinestore.controller.command;
 
 public enum CommandName {
-    LOGINATION, REGISTRATION, SAVENEWUSER, GOTOINDEXPAGE, GOTOMAINPAGE, LOGOUT
+    SIGN_IN, SIGN_UP, SAVE_NEW_USER, GO_TO_REGISTRATION_PAGE, GO_TO_INDEX_PAGE, LOGOUT
 }
--- src/main/java/by/logvin/onlinestore/controller/command/CommandProvider.java
+++ src/main/java/by/logvin/onlinestore/controller/command/CommandProvider.java
@@ -1,5 +1,7 @@
 package by.logvin.onlinestore.controller.command;
 
+import by.logvin.onlinestore.controller.command.impl.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,20 +9,17 @@ public class CommandProvider {
     private Map<CommandName, Command> commands = new HashMap<>();
 
     public CommandProvider() {
-        commands.put(CommandName.LOGINATION, new Logination());
-        commands.put(CommandName.REGISTRATION, new GoToRegistrationPage());
-        commands.put(CommandName.SAVENEWUSER, new SaveNewUser());
-        commands.put(CommandName.GOTOINDEXPAGE, new GoToIndexPage());
-        commands.put(CommandName.GOTOMAINPAGE, new GoToMainPage());
+        commands.put(CommandName.SIGN_IN, new SignIn());
+        commands.put(CommandName.SIGN_UP, new SignUp());
+        commands.put(CommandName.SAVE_NEW_USER, new SaveNewUser());
+        commands.put(CommandName.GO_TO_INDEX_PAGE, new GoToIndexPage());
+        commands.put(CommandName.GO_TO_REGISTRATION_PAGE, new GoToRegistrationPage());
         commands.put(CommandName.LOGOUT, new Logout());
     }
 
 
     public Command takeCommand(String name) {
-        CommandName commandName;
-
-        commandName = CommandName.valueOf(name.toUpperCase());
-
+        CommandName commandName = CommandName.valueOf(name.toUpperCase());
         return commands.get(commandName);
     }
 }
