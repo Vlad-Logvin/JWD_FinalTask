4487d02f8bd8a3bac8e77721fa57deec531952fc
Vlad Logvin <vladlogvin97@gmail.com>
2021-03-07 23:44:09 +0300

    implements Command

---
+++ src/main/java/by/logvin/onlinestore/controller/command/impl/SignIn.java
@@ -0,0 +1,15 @@
+package by.logvin.onlinestore.controller.command.impl;
+
+import by.logvin.onlinestore.controller.command.Command;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignIn implements Command {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
