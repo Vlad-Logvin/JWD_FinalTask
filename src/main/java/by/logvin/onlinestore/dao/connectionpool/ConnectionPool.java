a05f9112cad4797f86e04be1ab9797c1627ee960
Vlad Logvin <vladlogvin97@gmail.com>
2021-03-07 23:47:38 +0300

    delete unnecessary

--- src/main/java/by/logvin/onlinestore/dao/connectionpool/ConnectionPool.java
+++ src/main/java/by/logvin/onlinestore/dao/connectionpool/ConnectionPool.java
@@ -1,10 +1,5 @@
 package by.logvin.onlinestore.dao.connectionpool;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-
 public class ConnectionPool {
 
 }
